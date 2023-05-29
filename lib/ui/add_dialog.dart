9b917de1d4f12bf43621f58588f295637e74e7a5
NguyenBDat1001 <nguyendat20018@gmail.com>
2023-05-29 21:11:35 +0700

    add animated_dialogbox

--- lib/ui/add_dialog.dart
+++ lib/ui/add_dialog.dart
@@ -16,99 +16,188 @@ class _MyDialogState extends State<MyDialog> {
   Widget build(BuildContext context) {
     return AnimatedDialog(
       dialog: Dialog(
+        alignment: Alignment.topCenter,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.only(bottom: 10),
-                decoration: const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Color.fromARGB(154, 91, 91, 91),
-                      width: 1.7, // Độ dày của đường viền
+        child: Container(
+          padding: EdgeInsets.all(12),
+          width: double.infinity,
+          //height: double.infinity,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 10, top: 10,),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Color.fromARGB(154, 91, 91, 91),
+                        width: 1.7, // Độ dày của đường viền
+                      ),
                     ),
                   ),
+                  child: Row(
+                    children: [
+                      Icon(
+                        IconlyBroken.paperPlus,
+                        size: 32,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      TextWidget(
+                        text: "Tạo đơn hàng nhập kho",
+                        textSize: 23,
+                        color: null,
+                        isBold: true,
+                      ),
+                    ],
+                  ),
                 ),
-                child: Row(
-                  children: [
-                    Icon(
-                      IconlyBroken.paperPlus,
-                      size: 32,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    TextWidget(
-                      text: "Tạo đơn hàng nhập kho",
-                      textSize: 22,
-                      color: null,
-                      isBold: true,
-                    ),
-                  ],
+                SizedBox(height: 18),
+                Container(
+                  width: double.maxFinite,
+                  height: 500,
+                  child: ListView.builder(
+                    itemCount: 1,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextWidget(
+                              text: "Mã đơn hàng", textSize: 17, color: null),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          TextFormField(
+                            controller: _textEditingController,
+                            style: const TextStyle(fontSize: 18),
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Nhập mã đơn hàng(vd: 001)"),
+                          ),
+                          SizedBox(
+                            height: 18,
+                          ),
+                          TextWidget(
+                              text: "Tên đơn hàng", textSize: 17, color: null),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          TextField(
+                            controller: _textEditingController,
+                            style: const TextStyle(fontSize: 18),
+                            keyboardType: TextInputType.name,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Nhập tên đơn hàng(vd: Loa)"),
+                          ),
+                          SizedBox(
+                            height: 18,
+                          ),
+                          TextWidget(
+                              text: "Địa chỉ", textSize: 17, color: null),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          TextField(
+                            controller: _textEditingController,
+                            style: const TextStyle(fontSize: 18),
+                            keyboardType: TextInputType.name,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Nhập Địa chỉ(vd: Tp.HCM)"),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          TextWidget(
+                              text: "Khối lượng(Gram)",
+                              textSize: 17,
+                              color: null),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          TextField(
+                            controller: _textEditingController,
+                            style: const TextStyle(fontSize: 18),
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Nhập khối lượng(vd: 300)"),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          TextWidget(
+                              text: "chiều dài(Cm)", textSize: 17, color: null),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          TextField(
+                            controller: _textEditingController,
+                            style: const TextStyle(fontSize: 18),
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Nhập chiều dài(vd: 10)"),
+                          ),
+                          SizedBox(
+                            height: 18,
+                          ),
+                          TextWidget(
+                              text: "Chiều rộng(Cm)",
+                              textSize: 17,
+                              color: null),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          TextField(
+                            controller: _textEditingController,
+                            style: const TextStyle(fontSize: 18),
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Nhập Chiều rộng(vd: 30)"),
+                          ),
+                          SizedBox(
+                            height: 18,
+                          ),
+                          TextWidget(
+                              text: "Chiều cao(Cm)", textSize: 17, color: null),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          TextField(
+                            controller: _textEditingController,
+                            style: const TextStyle(fontSize: 18),
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Nhập Chiều cao(vd: 20)"),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
                 ),
-              ),
-              TextWidget(text: "Mã đơn hàng", textSize: 18, color: null),
-              SizedBox(
-                height: 6,
-              ),
-              TextFormField(
-                controller: _textEditingController,
-                style: const TextStyle(fontSize: 18),
-                keyboardType: TextInputType.number,
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Nhập mã đơn hàng(vd: 001)"),
-              ),
-              SizedBox(
-                height: 23,
-              ),
-              TextWidget(text: "Tên đơn hàng", textSize: 18, color: null),
-              SizedBox(
-                height: 6,
-              ),
-              TextField(
-                controller: _textEditingController,
-                style: const TextStyle(fontSize: 18),
-                keyboardType: TextInputType.name,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Nhập tên đơn hàng(vd: Loa)"),
-              ),
-              SizedBox(
-                height: 23,
-              ),
-              TextWidget(
-                  text: "đơn vị tính(Gram)", textSize: 18, color: null),
-              SizedBox(
-                height: 6,
-              ),
-              TextField(
-                controller: _textEditingController,
-                style: const TextStyle(fontSize: 18),
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Nhập đơn vị tính(vd: 300)"),
-              ),
-              
-              TextField(
-                controller: _textEditingController,
-                style: const TextStyle(fontSize: 18),
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Nhập đơn vị tính(vd: 300)"),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text("Lưu"),
-              ),
-            ],
+                SizedBox(
+                  height: 18,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Lưu"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
